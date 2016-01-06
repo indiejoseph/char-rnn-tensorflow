43f336f3cec615ac9b69900d58fe0ac7fd2707d6
Joseph Cheng <indiejoseph@Josephs-MacBook-Pro.local>
2016-01-06 11:54:30 +0800

    Update util

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ class TextLoader():
     self.data_dir = data_dir
     self.batch_size = batch_size
     self.seq_length = seq_length
-    main_input_file = os.path.join(data_dir, "input." + lang + ".txt")
+    self.tensors = dict()
     input_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir)
       if os.path.isfile(os.path.join(data_dir, f)) and os.path.splitext(f)[1] == ".txt"]
     vocab_file = os.path.join(data_dir, "vocab.pkl")
@@ -20,26 +20,23 @@ class TextLoader():
 
     if not (os.path.exists(vocab_file)) and not len(tensor_files):
       print "reading text file"
-      self.preprocess(main_input_file, input_files, vocab_file, tensor_files)
+      self.preprocess(input_files, vocab_file, tensor_files)
     else:
       print "loading preprocessed files"
-      self.load_preprocessed(vocab_file, tensor_files, lang)
-    self.create_batches()
+      self.load_preprocessed(vocab_file, tensor_files)
+    self.create_batches(lang)
     self.reset_batch_pointer()
 
-  def preprocess(self, main_input_file, input_files, vocab_file, tensor_files):
+  def preprocess(self, input_files, vocab_file, tensor_files):
     data = dict()
     langs = []
 
-    with codecs.open(main_input_file, "r", "utf-8") as f:
-      main_data = f.read()
-
     for input_file in input_files:
-      lang = re.match(r".+\/input\.([a-z]+)\.txt", input_file).group(1)
-      langs += [lang]
+      input_lang = re.match(r".+\/input\.([a-z]+)\.txt", input_file).group(1)
+      langs += [input_lang]
 
       with codecs.open(input_file, "r", "utf-8") as f:
-        data[lang] = f.read()
+        data[input_lang] = f.read()
 
     counter = collections.Counter("\n".join(data.values()))
     count_pairs = sorted(counter.items(), key=lambda x: -x[1])
@@ -50,13 +47,14 @@ class TextLoader():
     with open(vocab_file, "w") as f:
       cPickle.dump(self.chars, f)
 
-    self.tensor = np.array(map(self.vocab.get, main_data))
-
+    for _lang in langs:
+      self.tensors[_lang] = np.array(map(self.vocab.get, data[_lang]))
+    print self.tensors
     for key in data.keys():
       tensor_filename = "{}.{}.{}".format(os.path.join(self.data_dir, "data"), key, "npy")
       np.save(tensor_filename, np.array(map(self.vocab.get, data[key])))
 
-  def load_preprocessed(self, vocab_file, tensor_files, lang):
+  def load_preprocessed(self, vocab_file, tensor_files):
     with open(vocab_file) as f:
       self.chars = cPickle.load(f)
     self.vocab_size = len(self.chars)
@@ -64,19 +62,24 @@ class TextLoader():
 
     # get target dataset
     tensor_file = tensor_files[0]
+    tensors_size = 0
 
     for tf in tensor_files:
-      if "." + lang + ".npy" in tf:
-        tensor_file = tf
+      m_tensor = np.load(tf)
+      matches = re.match(r".+\/data\.([a-z]+)\.npy", tf)
+      if not matches:
+        continue
+      _lang = matches.group(1)
+      self.tensors[_lang] = m_tensor
+      tensors_size += m_tensor.size
 
-    self.tensor = np.load(tensor_file)
-    self.num_batches = self.tensor.size / (self.batch_size * self.seq_length)
+    self.num_batches = tensors_size / (self.batch_size * self.seq_length)
 
-  def create_batches(self):
-    self.num_batches = self.tensor.size / (self.batch_size * self.seq_length)
-    self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
-    xdata = self.tensor
-    ydata = np.copy(self.tensor)
+  def create_batches(self, lang):
+    tensors_size = self.tensors[lang].size
+    self.num_batches = tensors_size / (self.batch_size * self.seq_length)
+    xdata = self.tensors[lang][:self.num_batches * self.batch_size * self.seq_length]
+    ydata = np.copy(xdata)
     ydata[:-1] = xdata[1:]
     ydata[-1] = xdata[0]
     self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
